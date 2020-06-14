181c3d875848f21b4b587abf8ab8d49123f5a0d2
unknown <kazuihitoshi@gmail.com>
2020-06-14 20:47:38 +0900

    各種修正

--- dellsc02.py
+++ dellsc02.py
@@ -5,10 +5,33 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from html.parser import HTMLParser
 import urllib.parse 
+import datetime 
 
 targeturl ="https://www.dell.com/ja-jp/work/shop/%E3%83%87%E3%83%AB%E3%81%AE%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3/sc/laptops"
 outputfile = "dell.html"
 urltimeout = 60
+today = datetime.datetime.now().strftime('%Y/%m/%d')
+
+def gethead(today,totalcount):
+        ret ='<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">\n'
+        ret+='<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>\n'
+        ret+='<div class="separator" style="clear: both; text-align: center;"><a href="https://3.bp.blogspot.com/-6yNB7wFPCTE/XuXJWRHJ--I/AAAAAAAAj9Y/4zSkoQxkqLkVS5FMbearatX-zShMPhWEgCLcBGAsYHQ/s1600/dell%252Bubuntu.png" imageanchor="1" style="margin-left: 1em; margin-right: 1em;"><img border="0" src="https://3.bp.blogspot.com/-6yNB7wFPCTE/XuXJWRHJ--I/AAAAAAAAj9Y/4zSkoQxkqLkVS5FMbearatX-zShMPhWEgCLcBGAsYHQ/s320/dell%252Bubuntu.png" width="320" height="192" data-original-width="600" data-original-height="360" /></a></div>\n'
+        ret+='<meta name="twitter:card" content="summary_large_image" />\n'
+        ret+='<meta name="twitter:site" content="@kazuihitoshi" />\n'
+        ret+='<meta property="og:url" content="https://ubuntu84.blogspot.com/2020/05/dellubuntu-2020531.html" />\n'
+        ret+='<meta property="og:title" content="ubuntuノートパソコンがDELLで買ます 40機種を掲載 2020/6/14更新" />\n'
+        ret+='<meta property="og:description" content="なんと40機種もUbuntuプレインストール指定可能な製品が見つかりました。" />\n'
+        ret+='<meta property="og:image" content="https://3.bp.blogspot.com/-6yNB7wFPCTE/XuXJWRHJ--I/AAAAAAAAj9Y/4zSkoQxkqLkVS5FMbearatX-zShMPhWEgCLcBGAsYHQ/s1600/dell%252Bubuntu.png" />\n'
+        ret+='<h2>はじめに</h2>\n'
+        ret+='<p>DELLで見積もりを取る際に、たまにOSの選択肢にLinuxが表示されるものがあり、一体どの程度あるものか調べてみました。</p>\n'
+        ret+='<p>なんと40機種もUbuntuプレインストール指定可能な製品が見つかりました。</p>\n'
+        ret+='<p>それも嬉しいことに、Win10Proを選択した場合に比べ約2万円お安く購入可能です。</p>\n'
+        ret+='<p>皆様のLinuxライフにお役に立てると嬉しいです。それでは早速行ってみます。</p>\n'
+        ret+='<br>\n'
+        ret+='<h2>DELLで買えるUbuntuプレインストールパソコン</h2>\n'
+        ret+='<p>'+today+'の情報です。'+str(totalcount)+'機種を安価な順に並べています。</p>\n'        
+        return(ret)
+
 def pclist(soup):
         list=[]
         cnt = 0
@@ -95,6 +118,8 @@ for a in pclist(BeautifulSoup(urllib.request.urlopen(targeturl), "html.parser"))
 
 f = open(outputfile,encoding="utf-8",mode="w")
 linuxproduct = sorted(linuxproduct, key=lambda x:x['realprice'])
+h = gethead(today,len(linuxproduct)+1)
+f.write(h)
 for a in linuxproduct:
         d = a.get('productname')
         if d is not None :
@@ -133,3 +158,5 @@ for a in linuxproduct:
                 f.write(' </div>\n')
                 f.write('</div>\n')
 f.close()
+
+
